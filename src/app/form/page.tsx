983484913024a3473de439956e7c309ba2e28bc0
minjun-inpix <kkmj@inpix.com>
2023-11-01 17:07:51 +0900

    styled components

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -1,4 +1,30 @@
-// const formPage = () => {
-//   return ();
-// };
-// export default formPage;
+"use client";
+
+import Styled, { styled } from "styled-components";
+
+const Father = styled.div`
+  display: flex;
+`;
+
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
+  width: 100px;
+  height: 100px;
+`;
+
+const Circle = styled(Box)`
+    border-radius:50%;
+`;
+
+const formPage = () => {
+  return (
+    <>
+      <Father>
+        <Box bgColor="teal">Hello</Box>
+        <Circle bgColor="tomato" as="a">Hello</Circle>
+      </Father>
+    </>
+  );
+};
+
+export default formPage;
